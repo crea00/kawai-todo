2027791713017bf4b7746466a578c2d1e83ecf01
Jerome <654321su@gmail.com>
2018-12-10 00:00:08 +0900

    #06 Styling the ToDo Component & Add a ESLint rule

--- Todo.js
+++ Todo.js
@@ -23,19 +23,59 @@ export default class ToDo extends Component {
     });
   }
 
+  _startEditing = () => {
+    this.setState({
+      isEditing: true
+    });
+  }
+
+  _finishEditing = () => {
+    this.setState({
+      isEditing: false
+    });
+  }
+
   render() {
-    const { isCompleted } = this.state;
+    const { isCompleted, isEditing } = this.state;
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this._toggleComplete}>
-          <View 
-            style={[
-              styles.circle, 
-              isCompleted ? styles.completedCircle : styles.uncompletedCircle 
-            ]} 
-          />
-        </TouchableOpacity>
-        <Text style={styles.text}> Hello I'm a ToDo </Text>
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View 
+              style={[
+                styles.circle, 
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle 
+              ]} 
+            />
+          </TouchableOpacity>
+          <Text 
+            style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}
+          >
+            Hello I'm a ToDo 
+          </Text>
+        </View>
+        {isEditing ? (
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._finishEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>✅</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._startEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>✏️</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>❌</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
@@ -47,7 +87,8 @@ const styles = StyleSheet.create({
     borderBottomColor: '#bbb',
     borderBottomWidth: StyleSheet.hairlineWidth,
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
   circle: {
     width: 30,
@@ -67,5 +108,25 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 20,
     marginVertical: 20
+  },
+  completedText: {
+    color: '#bbb',
+    textDecorationLine: 'line-through'
+  },
+  uncompletedText: {
+    color: '#353535'
+  },
+  column: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: width / 2,
+    justifyContent: 'space-between'
+  },
+  actions: {
+    flexDirection: 'row',
+  },
+  actionContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10
   }
 });
